ee525de46d810fb3bbfe152d6124c262a1160441
suenalaba <joshuajr13@gmail.com>
2024-09-04 11:15:27 +0800

    feat: add obstacle class

---
+++ world/obstacle.py
@@ -0,0 +1,45 @@
+from math import pi
+
+from shared.constants import (
+    OBSTACLE_WIDTH,
+    ROBOT_MIN_CAMERA_DIST,
+    ROBOT_HEIGHT,
+    ROBOT_WIDTH
+)
+from shared.enums import Direction
+from shared.types import Position
+
+
+class Obstacle:
+    def __init__(
+        self,
+        x: float,
+        y: float,
+        facing: Direction
+    ):
+        self.x = x
+        self.y = y
+        self.facing = facing
+        self.middle = (x+(OBSTACLE_WIDTH/2), y+(OBSTACLE_WIDTH/2))
+
+
+    def get_interaction_position(self) -> Position:
+        '''
+        Returns the (x,y) position of the robot to interact with the obstacle. (E.g. to take a photo)
+        Refer to IPAD Notability for Diagram explaining this calculation
+        '''
+        x = self.x
+        y = self.y
+        theta = None
+
+        offset = (ROBOT_WIDTH - OBSTACLE_WIDTH) / 2
+
+        adjustments = {
+            Direction.NORTH: (offset + OBSTACLE_WIDTH, ROBOT_MIN_CAMERA_DIST + OBSTACLE_WIDTH + ROBOT_HEIGHT, -pi / 2),
+            Direction.SOUTH: (-offset, -(ROBOT_MIN_CAMERA_DIST + ROBOT_HEIGHT), pi / 2),
+            Direction.EAST: (ROBOT_MIN_CAMERA_DIST + OBSTACLE_WIDTH + ROBOT_WIDTH, -offset, pi),
+            Direction.WEST: (-(ROBOT_MIN_CAMERA_DIST + ROBOT_WIDTH), offset + OBSTACLE_WIDTH, 0)
+        }
+
+        dx, dy, theta = adjustments[self.facing]
+        return Position(x + dx, y + dy, theta)
